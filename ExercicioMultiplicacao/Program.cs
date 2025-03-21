a04d0a4ba2f7eb0318bab64703462c307df07d0f
tamyresrsm <tamyres.rsmagalhaes@senacsp.edu.br>
2025-03-20 21:44:01 -0300

    Adicionar arquivos de projeto.

---
+++ ExercicioMultiplicacao/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ExercicioMultiplicacao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escreva o primeiro número");
+            double numero1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Escreva o segundo número");
+            double numero2 = Convert.ToDouble(Console.ReadLine());
+
+            double multiplicao = numero1 * numero2;
+
+            Console.WriteLine("A multiplicação " + multiplicao.ToString("0.00"));
+
+        }
+    }
+}
